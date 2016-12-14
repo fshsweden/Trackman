40cb8668dc5a7ae0e2f70852f5485978c2ccee16
Peter Andersson <fshsweden@hotmail.com>
2016-12-14 14:22:50 +0100

    Added (non-working) Console Client

---
+++ Trackman.ConsoleClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using Trackman.Target.Interfaces;
+
+namespace Trackman.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var target = ActorProxy.Create<ITarget>(ActorId.CreateRandom(), "fabric:/Trackman/TargetActorService");
+
+            target.SetLocation()
+
+        }
+    }
+}
